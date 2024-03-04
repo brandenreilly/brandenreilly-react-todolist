ddcc0a66b1b1381f7211b8fb4daede5a0988d61c
brandenreilly <155998782+brandenreilly@users.noreply.github.com>
2024-03-04 14:45:32 +0000

    adding changes

--- src/js/component/todos.jsx
+++ src/js/component/todos.jsx
@@ -3,21 +3,30 @@ import React, {useRef, useEffect, useState} from "react";
 const ToDos = () => {
     const [newTasks,setNewTasks] = useState([])
     const [userInput,setUserInput] = useState("")
+    const [updatedTasks, setUpdatedTasks] = useState([])
     const addTasks = (newTask) => {
         var updatedTasks = [...newTasks]
         updatedTasks.push(newTask)
         setNewTasks(updatedTasks)
     }
     const deleteTask = (index) => {
-        var updatedTasks = newTasks.filter((element, ind)=>{
-            return ind != index
-        })
-        setNewTasks(updatedTasks)
-    }
+        var newUpdated = newTasks.filter((element, ind)=>{
+            if (ind != index){
+                setNewTasks(newUpdated);
+                return(
+                    <div className="row justify-content-center align-items-center">
+                        <div key={ind} className="taskDiv d-flex justify-content-between align-items-center">
+                            <p className="toDoTask">{element}</p>
+                            <i className="fas fa-trash deleteIcon" onClick={()=>{deleteTask()}}></i>
+                        </div>
+                    </div>
+                )
+            }
+    })}
     return (
         <div className="mt-5 entireToDoList row justify-content-center align-items-center">
             <div className="taskDiv align-items-center">
-                <input type="text" value={userInput} placeholder="What needs to be done?" 
+                <input type="text" id="inputTodo" value={userInput} placeholder="What needs to be done?" 
                 onChange={(e)=>{setUserInput(e.target.value)}} 
                 onKeyUp={(e)=>{if (e.key === "Enter"){addTasks(userInput);setUserInput("")}}} />
             </div>
